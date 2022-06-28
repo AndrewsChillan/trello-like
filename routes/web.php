c04aaefc4185c9a088582d147e7800c543b9c37e
AndrewsChillan <chillan.andrews@gmail.com>
2022-06-28 16:28:10 +0200

    commit 28.06.22 16:27

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\BController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProjetController;
 use App\Http\Controllers\ProfilController;
@@ -29,6 +28,18 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/trellos', [TrelloController::class, 'index']);
-Route::get('/projets', [ProjetController::class, 'index']);
+Route::get('/projets', [ProjetController::class, 'index'])
+    ->name('projets.index');
+Route::get('/projets/create', [ProjetController::class, 'create'])
+    ->name('projets.create');
+Route::post('/projets', [ProjetController::class, 'store'])
+    ->name('projets.store');
+Route::get('/projets/{id}/edit', [ProjetController::class, 'edit'])
+    ->name('projets.edit');
+Route::put('/projets/{id}', [ProjetController::class, 'update'])
+    ->name('projets/');
+Route::delete('/projets/{id}', [ProjetController::class, 'destroy'])
+    ->name('projets.destroy');
+
 Route::get('/profils/{id}/edit', [ProfilController::class, 'edit'])
     ->name('profils.edit');
