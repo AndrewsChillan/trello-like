4c1a0a189eafd9ddfe2306d3779b07f24f1e459e
Bastou06 <bastien.benni@gmail.com>
2022-06-30 13:56:24 +0200

    Maj page création projet

--- resources/views/trellos/create.blade.php
+++ resources/views/trellos/create.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.layout-trello')
 
 @section('trello create')
-    <form action="{{ route('trellos.store') }}" method="post">
+    <form class="createProjectForm" action="{{ route('trellos.store') }}" method="post">
     @csrf
+        <h2>Création d'un nouveau projet</h2>
+        <br>
+        <br>
+
+        <label>Entrez le nom de votre projet:</label>
         <input type="text" name="title" placeholder="Nom du projet">
-        <input type="submit" value="créer">
+
+        <br>
+        <br>
+        <br>
+
+        <p><button type="submit" class="btn btn-primary">Créer le projet</button></p>
     </form>    
 @endsection
