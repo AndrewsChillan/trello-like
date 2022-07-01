4b6c9076ef6e43c285ce7d3df3f9b0e02237dbf5
AmbivalenceR <ambivalence2806@gmail.com>
2022-07-01 15:44:38 +0200

    modif routes 2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\TrelloController;
